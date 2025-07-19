f9fa940e131c7ef3823ebaa8ce3e541ac9970987
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-07-19 21:06:13 +0200

    django project messaging app and

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+
+class User(AbstractUser):
+    """
+    Custom User model extending Django's AbstractUser
+    """
+    class Role(models.TextChoices):
+        GUEST = 'guest', 'Guest'
+        HOST = 'host', 'Host'
+        ADMIN = 'admin', 'Admin'
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        db_index=True
+    )
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+    email = models.EmailField(unique=True, null=False)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    role = models.CharField(
+        max_length=10,
+        choices=Role.choices,
+        default=Role.GUEST,
+        null=False
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    """
+    Model representing a message in a conversation
+    """
+    message_id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False,db_index=True)
+    sender_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    message_body = models.TimeField(max_length=225, null=False)
+
+
+class Conversation(models.Model):
+    """
+    Model representing a conversation between users
+    """
+    conversation_id = models.UUIDField(primary_key=True,default=uuid.uuid4, db_index=True)
+    participants_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)

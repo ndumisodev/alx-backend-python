5f15d2a372f62d2be5ce24b030a56c7d793392c5
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-07-19 20:37:46 +0200

    django project messaging app and

--- messaging_app1/messaging_app/messaging_app/settings.py
+++ messaging_app1/messaging_app/messaging_app/settings.py
@@ -44,10 +44,11 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ],
 }
 

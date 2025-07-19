808284122a1b11ff0e846f494a163d3bd35f1524
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-07-19 22:09:20 +0200

    django rest framework serializers

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,34 +1,31 @@
 from rest_framework import serializers
 from .models import User, Conversation, Message
 from django.contrib.auth.hashers import make_password
+from rest_framework.exceptions import ValidationError
 
 
 class UserSerializer(serializers.ModelSerializer):
-    """
-    Serializer for the User model with password handling
-    """
     class Meta:
         model = User
         fields = ['user_id', 'username', 'first_name', 'last_name', 'email', 'phone_number', 'role']
 
 
-
 class MessageSerializer(serializers.ModelSerializer):
-    """
-    Serializer for Message model with sender details
-    """
-    sender = UserSerializer(read_only=tuple, source="sender_id")
+    sender = UserSerializer(read_only=True, source='sender_id')
 
     class Meta:
         model = Message
-        fields = ["message_id", "sender", "message_body"]
-
+        fields = ['message_id', 'sender', 'message_body']
 
 
 class ConversationSerializer(serializers.ModelSerializer):
-    participants = UserSerializer(read_only=True, many=True)
-    messages = MessageSerializer(read_only=True, many=True, source='message_set')
+    participants = UserSerializer(many=True, read_only=True)
+    messages = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
         fields = ['conversation_id', 'participants', 'created_at', 'messages']
+
+    def get_messages(self, obj):
+        messages = Message.objects.filter(conversation=obj)
+        return MessageSerializer(messages, many=True).data

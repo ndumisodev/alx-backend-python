4ccf67c478df7457d7ce0066e2fc052e86d2212a
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-07-19 22:31:36 +0200

    django rest framework serializers

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -4,14 +4,21 @@ from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = ['user_id', 'username', 'first_name', 'last_name', 'email', 'phone_number', 'role']
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
 
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True, source='sender_id')
     message_body = serializers.CharField()
+    # queryset = Conversation.objects.prefetch_related('participants', Prefetch('messages', queryset=Message.objects.select_related('sender')))
 
     class Meta:
         model = Message
@@ -23,6 +30,13 @@ class MessageSerializer(serializers.ModelSerializer):
         if len(value) < 3:
             raise serializers.ValidationError("Message body must be at least 3 characters.")
         return value
+    
+    def get_messages(self, obj):
+        request = self.context.get('request')
+        messages = Message.objects.filter(conversation=obj)
+        page = self.paginate_queryset(messages, request)
+        serializer = MessageSerializer(page, many=True)
+        return serializer.data
 
 
 class ConversationSerializer(serializers.ModelSerializer):
@@ -36,3 +50,5 @@ class ConversationSerializer(serializers.ModelSerializer):
     def get_messages(self, obj):
         messages = Message.objects.filter(conversation=obj)
         return MessageSerializer(messages, many=True).data
+
+    read_only_fields = ['user_id', 'created_at'] 

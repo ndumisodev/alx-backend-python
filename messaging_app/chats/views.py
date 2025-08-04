428657471de4afe0c5516010c5c6bb89584e02c0
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-04 19:05:00 +0200

    django project

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework import filters
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.status import HTTP_403_FORBIDDEN
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 from .permissions import IsParticipantOfConversation
@@ -28,20 +28,17 @@ class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated, IsParticipantOfConversation]
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['message_body', 'sender__username']
 
-    def get_queryset(self):
-        # Filter to only messages in conversations the user is part of
-        return Message.objects.filter(conversation__participants=self.request.user)
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user not in instance.conversation.participants.all():
+            return Response({'detail': 'You do not have permission to update this message.'},
+                            status=HTTP_403_FORBIDDEN)
+        return super().update(request, *args, **kwargs)
 
-    def perform_create(self, serializer):
-        conversation_id = self.kwargs.get('conversation_pk')  # from nested URL
-        conversation = Conversation.objects.get(pk=conversation_id)
-
-        # Enforce user must be a participant
-        if self.request.user not in conversation.participants.all():
-            from rest_framework.exceptions import PermissionDenied
-            raise PermissionDenied("You are not a participant in this conversation.")
-
-        serializer.save(sender=self.request.user, conversation=conversation)
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user not in instance.conversation.participants.all():
+            return Response({'detail': 'You do not have permission to delete this message.'},
+                            status=HTTP_403_FORBIDDEN)
+        return super().destroy(request, *args, **kwargs)

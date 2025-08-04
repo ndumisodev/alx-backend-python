42eb0b0948b292b6d470a1b382a9ae40ebb1a8ce
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-04 19:01:56 +0200

    django project

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,24 +1,34 @@
 from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS
 
 class IsParticipantOfConversation(permissions.BasePermission):
     """
-    Allow only participants of a conversation to view, update, delete messages.
+    Allows access only to participants of a conversation for any action:
+    - View (GET)
+    - Update (PUT/PATCH)
+    - Delete (DELETE)
     """
 
     def has_permission(self, request, view):
-        # User must be authenticated
         return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         """
         Object-level permission:
-        - obj: Message or Conversation
-        - request.user must be one of the participants of obj.conversation
+        - obj can be a Message or Conversation
         """
+        user = request.user
+
+        # If obj is a Conversation
         if hasattr(obj, 'participants'):
-            # obj is a Conversation
-            return request.user in obj.participants.all()
-        elif hasattr(obj, 'conversation'):
-            # obj is a Message
-            return request.user in obj.conversation.participants.all()
+            return user in obj.participants.all()
+
+        # If obj is a Message
+        if hasattr(obj, 'conversation'):
+            conversation = obj.conversation
+
+            # Explicitly check for each HTTP method
+            if request.method in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
+                return user in conversation.participants.all()
+
         return False

60875227c37e7f0e7132d09bb3061a4a5f5ab19c
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-09 14:37:55 +0200

    django project

--- Django-signals_orm-0x04/chats/messaging/signals.py
+++ Django-signals_orm-0x04/chats/messaging/signals.py
@@ -1,37 +1,8 @@
-# messaging/signals.py
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Message, Notification
-from .models import Message, MessageHistory
-
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
-from .models import Message, MessageHistory
-
-
-
 
 @receiver(post_save, sender=Message)
-def create_notification(sender, instance, created, **kwargs):
+def create_notification_on_message(sender, instance, created, **kwargs):
     if created:
-        Notification.objects.create(
-            user=instance.receiver,
-            message=instance
-        )
-
-@receiver(pre_save, sender=Message)
-def log_message_edit(sender, instance, **kwargs):
-    if instance.pk:
-        try:
-            old_message = Message.objects.get(pk=instance.pk)
-        except Message.DoesNotExist:
-            return
-        
-        if old_message.content != instance.content:
-            # Log the old content with info who edited (use instance.sender or request user if accessible)
-            MessageHistory.objects.create(
-                message=old_message,
-                old_content=old_message.content,
-                edited_by=instance.sender  # assuming sender is the editor here
-            )
-            instance.edited = True
+        Notification.objects.create(user=instance.receiver, message=instance)

07785151fc0ecfb0a154f67a2d9bc06e3ffc1540
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-09 13:35:33 +0200

    django project

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,6 +1,8 @@
 # chats/middleware.py
 from datetime import datetime
 import logging
+from django.http import HttpResponseForbidden
+
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
@@ -19,3 +21,20 @@ class RequestLoggingMiddleware:
 
         response = self.get_response(request)
         return response
+
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Get current server time
+        current_hour = datetime.now().hour
+
+        # Allow only between 9 AM (09:00) and 6 PM (18:00)
+        if current_hour < 9 or current_hour >= 18:
+            return HttpResponseForbidden(
+                "Access to the chat is restricted to between 9 AM and 6 PM."
+            )
+
+        return self.get_response(request)
--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django_filter',
     'chats.middleware.RequestLoggingMiddleware',
     'apps.core.middleware.logging_middleware.RequestLoggingMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware',
 ]
 
 

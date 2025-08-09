3a426b76b88236d8805b579e84e0fa82276d5d66
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-09 14:23:12 +0200

    django project

---
+++ Django-signals_orm-0x04/messaging/apps.py
@@ -0,0 +1,9 @@
+# messaging/apps.py
+from django.apps import AppConfig
+
+class MessagingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'messaging'
+
+    def ready(self):
+        import messaging.signals

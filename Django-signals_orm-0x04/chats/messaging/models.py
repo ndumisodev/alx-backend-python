9ff39c3dcfae6d204ab61a4293764417e1c9da78
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-09 14:36:04 +0200

    django project

--- Django-signals_orm-0x04/chats/messaging/models.py
+++ Django-signals_orm-0x04/chats/messaging/models.py
@@ -1,33 +1,8 @@
-from django.db import models
-from django.contrib.auth.models import User
-import uuid
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Message, Notification
 
-class Message(models.Model):
-    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    edited = models.BooleanField(default=False)  # Track if message edited
-
-    def __str__(self):
-        return f"{self.sender.username}: {self.content[:20]}"
-
-class MessageHistory(models.Model):
-    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
-    old_content = models.TextField()
-    edited_at = models.DateTimeField(auto_now_add=True)  # When edited
-    edited_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)  # Who edited
-
-    def __str__(self):
-        return f"History for message {self.message.message_id} at {self.edited_at}"
-
-
-
-class Notification(models.Model):
-    user = models.ForeignKey(User, related_name="notifications", on_delete=models.CASCADE)
-    message = models.ForeignKey(Message, on_delete=models.CASCADE)
-    is_read = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Notification for {self.user} - Message ID: {self.message.id}"
+@receiver(post_save, sender=Message)
+def create_notification_on_message(sender, instance, created, **kwargs):
+    if created:
+        Notification.objects.create(user=instance.receiver, message=instance)
